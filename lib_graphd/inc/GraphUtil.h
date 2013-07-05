7fdfd0cb4c8996c62531c96b40f6cb233e56ba1c
Josh Lothian <lothian@ornl.gov>
2013-07-05 15:05:29 -0400

    Clean up new k-core code

--- lib_graphd/inc/GraphUtil.h
+++ lib_graphd/inc/GraphUtil.h
@@ -81,7 +81,7 @@ public:
     //Find the eccentricity of each vertex and store it in ecc.
     void find_ecc(Graph *g, vector<int> *ecc);
 
-    //Find the k-core number of each vertex and store it in kcore. 
+    //Find the k-core number of each vertex and store it in kcore.
     //Return degeneracy.
     int find_kcore(Graph *g, vector<int> *kcore);
 
