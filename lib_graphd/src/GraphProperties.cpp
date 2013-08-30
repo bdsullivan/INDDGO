2669a3801f738c8f66fee83945b9f1910f00701a
Blair D. Sullivan <sullivanb@ornl.gov>
2013-08-30 15:22:41 -0400

    Fixing OpenMP includes to work under Linux under all combinations of HAS_METIS and -fopenmp -D__OPENMP__. May now have issues under MSVC due to HAS_METIS always being defined.

--- lib_graphd/src/GraphProperties.cpp
+++ lib_graphd/src/GraphProperties.cpp
@@ -27,28 +27,6 @@
 #include <sched.h>
 #endif
 
-#ifdef _OPENMP
-  #include <omp.h>
-#else
-#ifdef HAS_METIS
-// CSG moving here from Graph.cpp... MSVC will complain
-// about multiple definitions with the #define omp_X as well
-void omp_set_num_threads(int num_threads) { return; }
-int omp_get_num_threads() { return 1; }
-int omp_get_max_threads(void) { return 1; }
-int omp_get_thread_num(void) { return 0; }
-int omp_get_num_procs(void) { return 1; }
-int omp_in_parallel(void) { return 0; }
-void omp_set_dynamic(int num_threads) { return; }
-int omp_get_dynamic(void) { return 0; }
-void omp_set_nested(int nested) { return; }
-int omp_get_nested(void) { return 0; }
-//      #define omp_get_num_threads() 1
-//      #define omp_get_thread_num() 0
-#endif
-#endif
-
-
 #include <numeric>
 
 namespace Graph {
